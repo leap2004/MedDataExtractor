7c20d8be8cc845a958a93a20e6da50db87470aee
leap2004 <109880758+leap2004@users.noreply.github.com>
2023-10-01 00:17:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from image_processor import ImageProcessor
 from text_extractor import TextExtractor
 from image_cleaner import ImageCleaner
 
+def get_user_input():
+    input_image_path = input("Please insert the path for inputted images: ")
+    output_image_path_noBackground = input("Please insert the path for output images with no background: ")
+    output_image_path_noText = input("Please insert the path for output images with no text: ")
+    output_text_path = input("Please insert path for outputted text file: ")
+    output_csv_path = input("Please insert path for outputted csv file: ")
+
+    return input_image_path, output_image_path_noBackground, output_image_path_noText, output_text_path, output_csv_path
+
 def process_images(input_image_path, output_image_path_noBackground, output_text_path, output_csv_path):
-    # Declaring input/output path directories
-    input_image_path=input("Please insert the path for inputted images: ")
-    output_image_path_noBackground=input("Please insert the path for output images with no background: ")
-    output_image_path_noText=input("Please insert the path for output images with no text: ")
-    output_text_path=input("Please insert path for outputted text file: ")
-    output_csv_path=input("Please insert path for outputted csv file: ")
-  
     image_processor = ImageProcessor(input_image_path, output_image_path_noBackground, output_text_path)
     text_extractor = TextExtractor(output_text_path, output_csv_path)
     image_cleaner = ImageCleaner(input_image_path, output_image_path_noText)
@@ -25,5 +27,8 @@ def process_images(input_image_path, output_image_path_noBackground, output_text
 
     # Generate the CSV with extracted information
     text_extractor.output_csv()
+
+if __name__ == "__main__":
+    input_image_path, output_image_path_noBackground, output_image_path_noText, output_text_path, output_csv_path = get_user_input()
 
     process_images(input_image_path, output_image_path_noBackground, output_text_path, output_csv_path)
